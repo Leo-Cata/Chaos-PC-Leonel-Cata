2e2ce6e0da13a4e0f798eae658e06e4607365dee
Leo-Cata <leonel.m.cata@gmail.com>
2022-09-12 21:42:08 -0300

    build fix

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 // import ItemListContainer from "./containers/ItemListContainer";
-// import {
-//   BrowserRouter,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
+import {
+  BrowserRouter,
+  // Routes,
+  // Route,
+} from "react-router-dom";
 // import NotFound from "./components/NotFound";
 import ItemDetailContainer from "./containers/ItemDetailContainer";
 
 function App() {
   const brandName = "Chaos PC";
   return (
-    <>
-      <NavBar brand ={brandName}/>
+    <BrowserRouter>
+      <NavBar 
+        brand ={brandName}
+      />
+      {/* <Routes>
+        <Route path="/" element={<ItemListContainer/>}/>
+        <Route path="/category/:categoryId" element={<ItemListContainer/>} />
+        <Route path="/detail/:productId" element={<ItemDetailContainer/> } />
+        <Route path="*" element={<NotFound/>} />
+      </Routes> */}
       <ItemDetailContainer/>
-      </>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<ItemListContainer/>}/>
-    //     <Route path="/category/:categoryId" element={<ItemListContainer/>} />
-    //     <Route path="/detail/:productId" element={<ItemDetailContainer/> } />
-    //     <Route path="*" element={<NotFound/>} />
-    //   </Routes>
-    //   </BrowserRouter>
+      </BrowserRouter>
   );
 }
 
