24200165f23a7470e175b4ed39066d4ff1997c30
Leo-Cata <leonel.m.cata@gmail.com>
2022-09-26 20:53:03 -0300

    nav cart with item count and only showing when there are items

--- src/components/CartWidget/index.jsx
+++ src/components/CartWidget/index.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { useContext } from 'react';
 import { TiShoppingCart } from 'react-icons/ti';
+import { useNavigate } from 'react-router-dom';
+import { CartCont } from '../../context/CartContext';
 import './style.scss';
 const CartWidget = () => {
-  return <TiShoppingCart className='menu-icon' />;
+  //get nav hook
+  const nav = useNavigate();
+
+  //nav to cart function
+  const handleNav = () => {
+    nav(`/cart`);
+  };
+
+  //cart context to get the amount of items in the cart
+  const { cart } = useContext(CartCont);
+
+  //if cart has any length render count and cart, else empty
+  return (
+    <div className='menu-Icon'>
+      {cart.length ? (
+        <>
+          <p className='menu-Count'>{cart.length}</p>
+          <TiShoppingCart className='menu-Items' onClick={handleNav} />
+        </>
+      ) : (
+        ''
+      )}
+    </div>
+  );
 };
 
 export default CartWidget;
--- src/containers/CartContainer/index.jsx
+++ src/containers/CartContainer/index.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import CartItem from '../../components/CartItem';
 import { CartCont } from '../../context/CartContext';
+import './styles.scss';
 
 const CartContainer = () => {
   const { cart, clearItems } = useContext(CartCont);
+
+  const nav = useNavigate();
+  const handleNav = () => {
+    nav(`/`);
+  };
   //get items in cart and map them and send them to CartItem
   return (
     <div>
-      <button onClick={clearItems}>asdasd</button>
-      {cart.map((cartProds) => {
-        return <CartItem key={cartProds.id} cartProds={cartProds} />;
-      })}
+      {cart.length ? (
+        ((<button onClick={clearItems}>asdasd</button>),
+        cart.map((cartProds) => {
+          return <CartItem key={cartProds.id} cartProds={cartProds} />;
+        }))
+      ) : (
+        <div className='cartNav'>
+          <h1 className='cart-Empty'>Tu Carrito Esta Vacio</h1>
+          <button className='cartNavButton' onClick={handleNav}>
+            Volver Al Inicio
+          </button>
+        </div>
+      )}
     </div>
   );
 };
