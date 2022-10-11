c27f7818d2692330cca24533b3f7b4497dae80e8
Leo-Cata <63213371+Leo-Cata@users.noreply.github.com>
2022-10-11 18:28:52 -0300

    mas imagenes de prodcs + small typo fix

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -15,7 +15,7 @@ const Item = ({ products }) => {
     <div className='card' onClick={handleNav}>
       <img className='cardImg' src={products.img} alt='asd' />
       <h1 className='cardTitle'>{products.name}</h1>
-      <p className='cardDesc'>{products.miniDesciption}</p>
+      <p className='cardDesc'>{products.miniDescription}</p>
       <p className='cardPrice'>${products.price}</p>
       <p className='cardStock'>Stock Disponible: {products.stock}</p>
     </div>
